fc9a65ae34a4915ccfa6e6103c07a2a57a5fa211
Ren <rpaczkowski@gmail.com>
2022-11-03 15:44:15 -0400

    Updated index file

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// require the express module
+import express from "express";
+
+// require the cors module
+import cors from "cors";
+
+// creates an instance of an Express server
+const app = express();
+
+// enable Cross Origin Resource Sharing so this API can be used from web-apps on other domains
+app.use(cors());
+
+// allow POST and PUT requests to use JSON bodies
+app.use(express.json());
+
+// define the port
+const port = 3000;
+
+// run the server
+app.listen(port, () => console.log(`Listening on port: ${port}.`));
