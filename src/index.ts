bcce8bcdf06a987de0d2dea5c42f2c7920d70ad3
Ren <rpaczkowski@gmail.com>
2022-11-03 15:57:35 -0400

    Connected to MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
-// require the express module
 import express from "express";
-
-// require the cors module
 import cors from "cors";
+import mongoose from "mongoose";
+import { config } from "./config/config";
 
-// creates an instance of an Express server
 const app = express();
 
-// enable Cross Origin Resource Sharing so this API can be used from web-apps on other domains
 app.use(cors());
-
-// allow POST and PUT requests to use JSON bodies
 app.use(express.json());
 
-// define the port
-const port = 3000;
+mongoose
+  .connect(config.mongoUri)
+  .then(() => console.log("Connected to MongoDB"));
 
-// run the server
+const port = config.port;
+
 app.listen(port, () => console.log(`Listening on port: ${port}.`));
